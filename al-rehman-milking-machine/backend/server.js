45a8e58c1d61fdc0a3eb5c4c8667e515f955bca9
Muhammad Hassan <ahsanhome212@gmail.com>
2026-08-14 23:49:23 +0500

    Allow www frontend in CORS

--- al-rehman-milking-machine/backend/server.js
+++ al-rehman-milking-machine/backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://alrahmanmilkingmachines.com",
+  "https://www.alrahmanmilkingmachines.com",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
